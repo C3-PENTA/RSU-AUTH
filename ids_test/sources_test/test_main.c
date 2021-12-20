daf74433dac5e525cfec4780d351af6dc2d498a6
YunKeon112 <kyun@pentasecurity.com>
2021-12-20 13:56:37 +0900

    ifdef NOT_USE_CAN_DRIVE

--- ids_test/sources_test/test_main.c
+++ ids_test/sources_test/test_main.c
@@ -41,8 +41,44 @@ int main(void)
 			idsSendMsgFromIDS();
 			SEMA42_DRV_UnlockGate(SEMA42_INSTANCE, SEMA42_GATE);
 		}
-		core0Count++;
+        		core0Count++;
 		idsDelay(100);
 	}
 #endif
-}
+
+#ifdef NOT_USE_CAN_DRIVER
+		int16_t Idx = (canIpcHdr->in + 1);
+		if(Idx!=canIpcHdr->out)
+		{
+			RTC_DRV_GetTimeDate(RTCTIMER1, &tempTime);
+			RTC_DRV_ConvertTimeDateToSeconds(&tempTime, &tempUSeconds);
+			tempSeconds = tempUSeconds/1000000;
+
+			if(core0Count == 0)
+			{
+				T = tempSeconds;
+				UT = tempUSeconds;
+			}
+			uint16_t Max = CAN_IPC_NUM;
+			uint16_t CanIdx = Idx % Max;
+			CanMsgInfo* canMsgInfo = (CanMsgInfo*)((uint8_t*)(canIpcHdr + 1) + CanIdx*HIDS_MSG_LEN);
+			canMsgInfo->mark = 0x5A5A;
+			canMsgInfo->bus = (uint8_t) 1;
+			canMsgInfo->dlc = (uint8_t) 8;
+			canMsgInfo->mid = (uint32_t) 2;
+			canMsgInfo->sec = tempSeconds - T;
+			canMsgInfo->usec = tempUSeconds - UT;
+			for(int i = 0 ; i < (int)canMsgInfo->dlc ; i++ )
+			{
+				canMsgInfo->data[i] = (uint8_t) 17;
+			}
+			canIpcHdr->in = CanIdx;
+		}
+
+		/*Semaphore Unlock*/
+		SEMA42_DRV_UnlockGate(SEMA42_INSTANCE, SEMA42_GATE);
+	   }
+       	   core0Count++;
+	   idsDelay(10);
+	}
+#endif
