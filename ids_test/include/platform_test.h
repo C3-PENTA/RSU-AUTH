de0a6cf8d1d641ef172ca57dcaa402ba1ea39ad4
YunKeon112 <kyun@pentasecurity.com>
2021-12-22 09:57:39 +0900

    add CAN Data Save

--- ids_test/include/platform_test.h
+++ ids_test/include/platform_test.h
@@ -12,3 +12,13 @@ typedef struct {
   __IO uint16_t in;		  /*Ring BUFFER HEADER*/
   __IO uint16_t out;	  /*RING BUFFER TAIL*/
 } HidsIpcHdr_Type;
+
+typedef struct {
+   __IO uint16_t mark;    /*DEFAULT 0x5A5A*/
+   __IO uint8_t  bus;     /* BUS NUM, BCAN-0x00, CCAN-0x01, ICAN-0x03, MMCAN-0x04, PCAN-0x05*/
+   __IO uint8_t  dlc;     /*DATA LENGHT*/
+   __IO uint32_t mid;     /*ARBITRATION ID*/
+   __IO uint32_t sec;     /*TIME(second)*/
+   __IO uint32_t usec;    /*TIME(microsecond)*/
+   __IO uint8_t  data[8]; /*MSG*/
+} CanMsgInfo_Type;
