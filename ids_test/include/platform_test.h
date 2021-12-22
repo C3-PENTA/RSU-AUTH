d220dcc6de8be5ae263a59d4781d70fc94952392
YunKeon112 <kyun@pentasecurity.com>
2021-12-22 09:51:00 +0900

    update platform header shared memory test

--- ids_test/include/platform_test.h
+++ ids_test/include/platform_test.h
@@ -1,2 +1,9 @@
 #if !defined(IDS_PLATFORM_COMMON_H)
 #define IDS_PLATFORM_COMMON_H
+
+#include "cstdint"
+/* IO definitions (access restrictions to peripheral registers) */
+
+typedef struct {
+	__IO uint16_t NUM;          /*NUM OF RECEIVED MSG*/
+} TEST_Type;
