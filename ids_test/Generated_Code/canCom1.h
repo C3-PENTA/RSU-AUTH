07603f925d200845a2444568c66a138e0341f72d
YunKeon112 <kyun@pentasecurity.com>
2021-12-22 13:09:18 +0900

    Update canCom1

--- ids_test/Generated_Code/canCom1.h
+++ ids_test/Generated_Code/canCom1.h
@@ -4,4 +4,10 @@
 /* Include inherited beans */
 #include "Cpu.h"
 
+/*! @brief Device instance number */
+#define INST_CANCOM1 (0U)
+/*! @brief Driver state structure which holds driver runtime data */
+extern flexcan_state_t canCom1_State; 
+extern const flexcan_user_config_t canCom1_InitConfig0;
+
 #endif
