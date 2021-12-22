08c67653d5fe00553a3517e1858b755928bab7cd
YunKeon112 <kyun@pentasecurity.com>
2021-12-22 10:20:12 +0900

    Updaqte Cpu header

--- ids_test/Generated_Code/Cpu.h
+++ ids_test/Generated_Code/Cpu.h
@@ -1,2 +1,16 @@
 #ifndef Cpu_H
 #define Cpu_H
+
+/*Include shared modules, which are used for whole project*/
+#include "device_registers.h"
+#include "interrupt_manager.h"
+#include "clock.h"
+#include "pit_driver.h"
+#include "edma_driver.h"
+#include "osif.h"
+#include "flexcan_driver.h"
+#include "sema42_driver.h"
+#include "rtc_api_driver.h"
+#include "system_MPC5748G.h"
+
+#endif
