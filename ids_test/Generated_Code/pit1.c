477d5d1cca59db3e7e1d40681202f98245a2bd77
YunKeon112 <kyun@pentasecurity.com>
2021-12-27 14:22:43 +0900

    Add User channel configureation

--- ids_test/Generated_Code/pit1.c
+++ ids_test/Generated_Code/pit1.c
@@ -3,6 +3,15 @@
 const pit_config_t pit1_InitConfig =
 {
     .enableStandardTimers = true,
-    .enableRTITimer = false,
-    .stopRunInDebug = false
+    .enableRTITimer = true,
+    .stopRunInDebug = true
+};
+
+pit_channel_config_t pit1_ChnConfig0 =
+{
+    .hwChannel = 0U,
+    .periodUnit = PIT_PERIOD_UNITS_MICROSECONDS,
+    .period = 500000U,
+    .enableChain = false,
+    .enableInterrupt = true
 };
