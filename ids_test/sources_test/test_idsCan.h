26cbf191d98aaefe6d878bfbc65fe999fa18a45b
YunKeon112 <kyun@pentasecurity.com>
2021-12-27 13:38:00 +0900

    update test_idscan header

--- ids_test/sources_test/test_idsCan.h
+++ ids_test/sources_test/test_idsCan.h
@@ -3,6 +3,21 @@
 
 #include "test_canCom1.h"
 
+/*CAN DEFINE*/
+#define MASTER
+
+#if defined(MASTER)
+    #define TX_MAILBOX  (1UL)
+    #define TX_MSG_ID   (1UL)
+    #define RX_MAILBOX  (0UL)
+    #define RX_MSG_ID   (2UL)
+#elif defined(SLAVE)
+    #define TX_MAILBOX  (0UL)
+    #define TX_MSG_ID   (2UL)
+    #define RX_MAILBOX  (1UL)
+    #define RX_MSG_ID   (1UL)
+#endif
+
 struct idsCanMsgBuf
 {
 	flexcan_msgbuff_t msgBuf;
