532b5fcad618c1e39655abbe4d75757d6134e43c
YunKeon112 <kyun@pentasecurity.com>
2021-12-27 13:18:37 +0900

    Update ExdataInfo

--- ids_test/sources_test/test_idsCan.c
+++ ids_test/sources_test/test_idsCan.c
@@ -19,6 +19,15 @@ void idsSendCanData(uint8_t inst, uint32_t mailbox, uint32_t messageId, uint8_t 
             .fd_padding  = 0U
     };
 
+    flexcan_data_info_t RXdataInfo =
+    {
+		.data_length = 8U,
+		.msg_id_type = FLEXCAN_MSG_ID_STD,
+		.enable_brs  = true,
+		.fd_enable   = true,
+		.fd_padding  = 0U
+    };
+
     /* Execute send non-blocking */
     if(messageId != 0x4c1 || messageId != 0x4c2)
     {
