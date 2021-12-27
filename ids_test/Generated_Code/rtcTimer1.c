06c02ebc286c2cdeee25f51b0d05b7fe5c866a60
YunKeon112 <kyun@pentasecurity.com>
2021-12-27 14:42:55 +0900

    Init rtcTimer1.c

---
+++ ids_test/Generated_Code/rtcTimer1.c
@@ -0,0 +1,11 @@
+rtc_state_t rtcTimer1_State;
+
+/*! rtcTimer1 configuration structure */
+rtc_init_config_t rtcTimer1_Config0 = 
+{
+    .clockSelect                =   RTC_CLOCK_SOURCE_FIRC, 
+    .divideBy32                 =   false,
+    .divideBy512                =   false,
+    .freezeEnable               =   false,
+    .nonSupervisorAccessEnable  =   true
+};
